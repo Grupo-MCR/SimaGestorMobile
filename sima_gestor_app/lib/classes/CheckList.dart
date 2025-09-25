430a5693e8b3bbeac23048e44fced3a6f6a3cdb0
gustagueimer <gustavomarqueslima@outlook.com>
2025-09-25 10:05:30 -0300

    fix: implementação de getters e setters, metódo para retornar o map para comunicar com a api e outras adaptações

--- sima_gestor_app/lib/classes/CheckList.dart
+++ sima_gestor_app/lib/classes/CheckList.dart
@@ -1,22 +1,58 @@
+import 'package:http/http.dart';
+import 'package:sima_gestor_app/classes/veiculo.dart';
+
+import 'item_checklist.dart';
+
 class CheckList {
-  int id;
-  DateTime checkListDate;
-  List<ItemCheckList> itemVerificados;
+  int? id;
+  int? idVeiculo;
+  int? idMotorista;
+  String? nomeVerificador;
+  String? assinaturaBase64;
+  List<ItemCheckList> itens = [];
 
   // Construtor
   CheckList({
-    required this.id,
-    required this.checkListDate,
-    required this.itemVerificados,
+    this.id,
+    this.idVeiculo,
+    this.idMotorista,
+    this.nomeVerificador,
+    this.assinaturaBase64
   });
 
-  // Método para buscar os dados (simulação)
-  void buscarDados() {
-    print("Buscando dados do checklist $id...");
+  bool validarNull() {
+    if(idVeiculo == null ||
+        idMotorista == null ||
+        nomeVerificador == null || 
+        assinaturaBase64 == null ||
+        itens.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+
+  Map<String, dynamic> buildChecklist() {
+    if(validarNull() == false) {
+      throw new ArgumentError.notNull("argumentos nulos");
+    }
+    Map<String, dynamic> checklist = {};
+    checklist['vehicle_id'] = idVeiculo;
+    checklist['driver_id'] = idMotorista;
+    checklist['checker_name'] = nomeVerificador;
+    checklist['signature'] = assinaturaBase64;
+    for(int i=0; i<itens.length; i++) {
+      Map<String, dynamic> item = itens[i].buildItem();
+      checklist["items[" + item['id'] + "][result]"]= item['result'];
+      checklist["items[" + item['id'] + "][comments]"]= item['comments'];
+      if(item['result'] ==  "not_ok") {
+        checklist["items[" + item['id'] + "][photo]"]= item['photo'];
+      }
+    }
+    return checklist;
   }
 
   @override
   String toString() {
-    return 'CheckList{id: $id, data: $checkListDate, itens: $itemVerificados}';
+    return 'CheckList{id: $id, idVeiculo: $idVeiculo, idMotorista: $idMotorista, nomeVerificador: $nomeVerificador, assinaturaBase64: $assinaturaBase64, itens: $itens}';
   }
 }
