6bedaef6f7a44754a417a4d8f21f7951f5a9aa01
gustagueimer <gustavomarqueslima@outlook.com>
2025-09-26 21:17:13 -0300

    fix: adição de um print do status code da request caso a mesma falhe

--- sima_gestor_app/lib/classes/fetch.dart
+++ sima_gestor_app/lib/classes/fetch.dart
@@ -17,6 +17,7 @@ class Fetch {
 
     var response = await client.get(fetchUrl, headers: fetchHeaders); // Execução da requisição
     if(response.statusCode != 200) {
+      print(response.statusCode);
       throw Exception('request failed :p'); // Lança erro se a requisição não for bem sucedida :P
     }
     return jsonDecode(response.body); // Retorno da resposta da requisição
@@ -30,6 +31,7 @@ class Fetch {
 
     var response = await client.post(fetchUrl, body: fetchBody, headers: fetchHeaders); // Execução da requisição
     if(response.statusCode != 200) {
+      print(response.statusCode);
       throw Exception('request failed :p'); // Lança erro se a requisição não for bem sucedida :P
     }
     return jsonDecode(response.body); // Retorno da resposta da requisição
