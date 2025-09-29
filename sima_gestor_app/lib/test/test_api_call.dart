d026f0c033ed3eaa61fd335ae5e1cb64cb0e4092
gustagueimer <gustavomarqueslima@outlook.com>
2025-09-29 15:20:55 -0300

    feat: adição de teste do método de enviar abastecimento

--- sima_gestor_app/lib/test/test_api_call.dart
+++ sima_gestor_app/lib/test/test_api_call.dart
@@ -1,4 +1,5 @@
 import '../classes/usuario.dart';
+import '../classes/abastecimento.dart';
 import '../classes/api_call.dart';
 
 void main() async{
@@ -25,6 +26,24 @@ void main() async{
   }
   print("\nlista de motoristas: ");
   print(rm);
+
+  dynamic rv = await api.receberVeiculos();
+  if(rv == null) {
+    print("test failed :c");
+    return;
+  }
+  print("\nlista de veiculos: ");
+  print(rv);
+
+  Abastecimento abastecimento = Abastecimento('SUS2O20', DateTime.now(), 333.33, 'Energia', 0.20, 5000);
+  print(abastecimento);
+  dynamic ra = await api.enviarAbastecimento(abastecimento.buildAbastecimento());
+  if(ra == null) {
+    print("test failed :c");
+    return;
+  }
+  print('\nabastecimento: ');
+  print(ra);
 
   print("test success :D");
 }
