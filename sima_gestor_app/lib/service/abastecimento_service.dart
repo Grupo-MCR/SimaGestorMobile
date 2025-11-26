dd4091435bf7a55957f4262ddbac2d06e68250ff
gustagueimer <gustavomarqueslima@outlook.com>
2025-11-26 11:04:33 -0300

    fix: adaptações menores sobre a validação dos campos

--- sima_gestor_app/lib/service/abastecimento_service.dart
+++ sima_gestor_app/lib/service/abastecimento_service.dart
@@ -7,17 +7,15 @@ class AbastecimentoService {
     String token,
     Abastecimento abastecimento) async {
       if(servidor.isEmpty || token.isEmpty) {
-        throw new Exception("Erro: não foi possível realizar conexão com o sistema");
+        throw Exception("Erro: não foi possível realizar conexão com o sistema");
       }
-      if(!abastecimento.validarNull()) {
-        throw new Exception("Erro: um ou mais campos não preenchidos");
-      }
+      abastecimento.validarAll();
       APICall api = APICall(servidor, token);
       try {
         var respostaAPI = await api.enviarAbastecimento(abastecimento.buildAbastecimento());
         return respostaAPI;
       } catch (e) {
-        throw new Exception(e.toString());
+        throw Exception(e.toString());
       }
   }
 }
